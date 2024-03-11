1135b22cee0530429dc37f23c6e797e495586e20
Kooreis <kooreis.official@gmail.com>
2024-03-11 18:37:53 -0500

    This is the main class that creates an instance of the CustomDataStructure class and tests its methods. It inserts four integers, searches for one of them, deletes another, and then calls the getRandom method.

--- solution.java
+++ solution.java
@@ -1,6 +1,3 @@
-Here is a Java console application that implements a data structure supporting insert, delete, search, and getRandom operations in constant time. This is achieved by using a combination of ArrayList and HashMap.
-
-```java
 import java.util.*;
 
 public class Main {
@@ -15,46 +12,3 @@ public class Main {
         System.out.println(ds.getRandom());
     }
 }
-
-class CustomDataStructure {
-    ArrayList<Integer> arr;
-    HashMap<Integer, Integer> hash;
-
-    public CustomDataStructure() {
-        arr = new ArrayList<Integer>();
-        hash = new HashMap<Integer, Integer>();
-    }
-
-    void insert(int x) {
-        if (hash.get(x) != null)
-            return;
-        int s = arr.size();
-        arr.add(x);
-        hash.put(x, s);
-    }
-
-    void delete(int x) {
-        Integer index = hash.get(x);
-        if (index == null)
-            return;
-        hash.remove(x);
-        int size = arr.size();
-        Integer last = arr.get(size - 1);
-        Collections.swap(arr, index,  size - 1);
-        arr.remove(size - 1);
-        hash.put(last, index);
-    }
-
-    boolean search(int x) {
-        return hash.containsKey(x);
-    }
-
-    int getRandom() {
-        Random rand = new Random();
-        int index = rand.nextInt(arr.size());
-        return arr.get(index);
-    }
-}
-```
-
-In this code, the `CustomDataStructure` class uses an `ArrayList` and a `HashMap` to support the required operations. The `ArrayList` is used to support `getRandom` in O(1) time, and the `HashMap` is used to support `insert`, `delete`, and `search` in O(1) time. The `insert` method adds an element to the end of the `ArrayList` and updates the `HashMap` with the element's value and its index in the `ArrayList`. The `delete` method removes an element from the `HashMap` and replaces its position in the `ArrayList` with the last element in the `ArrayList`. The `search` method checks if an element is in the `HashMap`. The `getRandom` method generates a random index and returns the element at that index in the `ArrayList`.
