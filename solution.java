45d22b2b61bee967e1b5f4bbe685dd918b54aecc
Kooreis <kooreis.official@gmail.com>
2024-03-12 18:37:53 -0500

    This is the CustomDataStructure class with its constructor. It initializes an ArrayList and a HashMap to be used for the operations.

--- solution.java
+++ solution.java
@@ -1,14 +1,9 @@
-import java.util.*;
+class CustomDataStructure {
+    ArrayList<Integer> arr;
+    HashMap<Integer, Integer> hash;
 
-public class Main {
-    public static void main(String[] args) {
-        CustomDataStructure ds = new CustomDataStructure();
-        ds.insert(10);
-        ds.insert(20);
-        ds.insert(30);
-        ds.insert(40);
-        System.out.println(ds.search(30));
-        ds.delete(20);
-        System.out.println(ds.getRandom());
+    public CustomDataStructure() {
+        arr = new ArrayList<Integer>();
+        hash = new HashMap<Integer, Integer>();
     }
 }
